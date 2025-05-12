01d11ffb63f7d3ffb18ec1fe61d7238aff5b3e42
StilaYN <dist.avdeev2004@gmail.com>
2025-05-12 10:42:55 +0700

    Изменена версия модели

--- app/src/main/java/com/example/navigatorforvisuallyimpaired/Constants.kt
+++ app/src/main/java/com/example/navigatorforvisuallyimpaired/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.navigatorforvisuallyimpaired
 
 object Constants {
-    const val MODEL_PATH = "model.tflite"
+    const val MODEL_PATH = "prod_model.tflite"
     const val LABELS_PATH = "labels.txt"
 }
